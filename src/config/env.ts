e31f38e02fb0bf88d97dcb3fc5082786227a6665
mybox14 <mybox1407@gmail.com>
2026-09-11 10:59:32 +0400

    Update env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const env = {
-  port: Number(process.env.PORT) || 3002,
+  port: Number(process.env.PORT) || 3005,
   apiKey: process.env.API_KEY || '',
   apiSecret: process.env.API_SECRET || '',
   nodeEnv: process.env.NODE_ENV || 'development',
