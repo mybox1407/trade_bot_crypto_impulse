8a1241c060bfb5cd49a2cbef4584e2e5467d4040
mybox14 <mybox1407@gmail.com>
2026-09-11 11:19:00 +0400

    Update check-lighter-exchange.ts

--- check-lighter-exchange.ts
+++ check-lighter-exchange.ts
@@ -5,22 +5,38 @@ import {
   getMarketPrice
 } from './src/services/exchange';
 
-startMarketData('ETH/USDT', '15m');
+async function main() {
+  await startMarketData('ETH/USDT', '15m');
 
-const timer = setInterval(() => {
-  const candles = getCandles('ETH/USDT', '15m', 10);
-  const price = getCurrentPrice('ETH/USDT');
-  const marketPrice = getMarketPrice('ETH/USDT');
+  const timer = setInterval(() => {
+    const candles = getCandles(
+      'ETH/USDT',
+      '15m',
+      250
+    );
 
-  console.log({
-    candlesCount: candles.length,
-    lastCandle: candles.at(-1),
-    price,
-    marketPrice
-  });
-}, 5_000);
+    const price = getCurrentPrice('ETH/USDT');
+    const marketPrice = getMarketPrice('ETH/USDT');
 
-setTimeout(() => {
-  clearInterval(timer);
-  process.exit(0);
-}, 30_000);
+    console.log({
+      candlesCount: candles.length,
+      firstCandle: candles[0],
+      lastCandle: candles.at(-1),
+      price,
+      marketPrice
+    });
+  }, 5_000);
+
+  setTimeout(() => {
+    clearInterval(timer);
+    process.exit(0);
+  }, 30_000);
+}
+
+main().catch(error => {
+  console.error(
+    error instanceof Error ? error.message : error
+  );
+
+  process.exit(1);
+});
