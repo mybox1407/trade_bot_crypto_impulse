4a52f97b238ef5e74c53c033d747a8641c83bb7b
mybox14 <mybox1407@gmail.com>
2026-09-24 18:10:45 +0400

    Update strategy.ts

--- src/services/strategy.ts
+++ src/services/strategy.ts
@@ -38,9 +38,9 @@ export const MIN_LAST_ATR_PCT = 0.005;
 export const MAX_LAST_ATR_PCT_LONG = 5; //Было 0.0195
 export const MAX_LAST_ATR_PCT_SHORT = 5; //Было 0.025
 
-export const MIN_BB_WIDTH_LONG = 0.05; //Было 0.053
+export const MIN_BB_WIDTH_LONG = 0.04; //Было 0.053
 export const MAX_BB_WIDTH_LONG = 0.090;
-export const MIN_BB_WIDTH_SHORT = 0.05;
+export const MIN_BB_WIDTH_SHORT = 0.04; //Было 0.05
 
 export const MIN_ENTRY_DISTANCE_FROM_EMA20_PERCENT = 90;
 export const MIN_ENTRY_DISTANCE_FROM_EMA20_ATR =
