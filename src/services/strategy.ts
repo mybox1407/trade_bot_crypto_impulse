1cb122f0234c43b103377bbc5d214d25c94bc475
mybox14 <mybox1407@gmail.com>
2026-09-26 08:41:56 +0400

    Update strategy.ts

--- src/services/strategy.ts
+++ src/services/strategy.ts
@@ -25,27 +25,27 @@ const TRADING_HOUR_WINDOWS_UTC_PLUS_4: ReadonlyArray<readonly [number, number]> 
  
 ];
 
-export const MIN_ENTRY_RSI_SHORT = 38; //Было 39
-export const MAX_ENTRY_RSI_SHORT = 43; //Было 42
-export const MIN_ENTRY_RSI_LONG = 50; //Было 51
-export const MAX_ENTRY_RSI_LONG = 66; //Было 64
+export const MIN_ENTRY_RSI_SHORT = 38; 
+export const MAX_ENTRY_RSI_SHORT = 42; 
+export const MIN_ENTRY_RSI_LONG = 51;
+export const MAX_ENTRY_RSI_LONG = 64;
 
 export const MIN_ENTRY_ADX_SHORT = 25;
-export const MIN_ENTRY_ADX_LONG = 25; //Было 29.5
+export const MIN_ENTRY_ADX_LONG = 28; 
 export const MAX_ENTRY_ADX = 40;
 
 export const MIN_LAST_ATR_PCT = 0.005;
-export const MAX_LAST_ATR_PCT_LONG = 5; //Было 0.0195
-export const MAX_LAST_ATR_PCT_SHORT = 5; //Было 0.025
+export const MAX_LAST_ATR_PCT_LONG = 0.0195;
+export const MAX_LAST_ATR_PCT_SHORT = 0.025;
 
-export const MIN_BB_WIDTH_LONG = 0.04; //Было 0.053
+export const MIN_BB_WIDTH_LONG = 0.05; 
 export const MAX_BB_WIDTH_LONG = 0.090;
-export const MIN_BB_WIDTH_SHORT = 0.04; //Было 0.05
+export const MIN_BB_WIDTH_SHORT = 0.05;
 
 export const MIN_ENTRY_DISTANCE_FROM_EMA20_PERCENT = 90;
 export const MIN_ENTRY_DISTANCE_FROM_EMA20_ATR =
   MIN_ENTRY_DISTANCE_FROM_EMA20_PERCENT / 100;
-export const MAX_ENTRY_EXTENSION_TREND_ATR = 1.8; //Было 1.2
+export const MAX_ENTRY_EXTENSION_TREND_ATR = 1.5;
 export const REJECT_ENTRY_TOO_EXTENDED = true;
 
 // Blacklist только для Long
